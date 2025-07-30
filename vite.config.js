e1d0b2511197c0714855c102f0940054ffca460b
Shittu Michael <macmorgany@gmail.com>
2025-07-29 18:18:35 -0700

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 100000000
-    },
-    base: "/react_portfolio_site/",
+  build: {
+    chunkSizeWarningLimit: 100000000
+  },
+  base: "/react_portfolio_site/"
 })
