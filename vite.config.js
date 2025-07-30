8740838acf6e58885eeafdc3e66674bbe3f645b5
Shittu Michael <macmorgany@gmail.com>
2025-07-29 18:25:38 -0700

    Fix vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 100000000
+    chunkSizeWarningLimit: 100000000,
   },
-  base: "/react_portfolio_site/"
-})
+  base: '/react_portfolio_site/',
+});
