cbb8304ef1c496d994c6671e12f3560c1c228d66
Shittu Michael <macmorgany@gmail.com>
2025-07-31 05:28:19 -0700

    tracked

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
@@ -18,12 +19,12 @@ const Computers = ({ isMobile }) => {
         shadow-mapSize={1024}
       />
       <pointLight intensity={1} />
-      <primitive
+      {/* <primitive
         object={computer.scene}
         scale={isMobile ? 0.6 : 0.7}
         position={isMobile ? [0, -4.5, -2.2] : [0, -4.5, -3.5]}
         rotation={[-0.01, -0.2, -0.1]}
-      />
+      /> */}
     </mesh>
   );
 };
@@ -32,16 +33,24 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 640px)");
+
+    // Set the initial value of the isMobile state variable
     setIsMobile(mediaQuery.matches);
 
+    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
 
+    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-    return () =>
+
+    // Remove the listener when the component is unmounted
+    return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
   }, []);
 
   return (
@@ -51,12 +60,6 @@ const ComputersCanvas = () => {
       dpr={[1, 2]}
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
-      style={{
-        width: "100vw",
-        height: "100vh",
-        position: "relative", // or "absolute" depending on your layout
-        overflow: "hidden", // ensure no horizontal scroll
-      }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
@@ -66,6 +69,7 @@ const ComputersCanvas = () => {
         />
         <Computers isMobile={isMobile} />
       </Suspense>
+
       <Preload all />
     </Canvas>
   );
